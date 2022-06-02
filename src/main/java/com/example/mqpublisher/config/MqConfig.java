19a9e3c03c580cd13dd3b03a5ce11853f1d07234
jisu0309 <sksfh0147@gamil.com>
2022-06-02 14:35:22 +0900

    add ampqAdmin for createQueue

--- src/main/java/com/example/mqpublisher/config/MqConfig.java
+++ src/main/java/com/example/mqpublisher/config/MqConfig.java
@@ -2,6 +2,7 @@ package com.example.mqpublisher.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -34,6 +35,11 @@ public class MqConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
         return rabbitTemplate;
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
     }
 
 
--- src/main/java/com/example/mqpublisher/controller/MqController.java
+++ src/main/java/com/example/mqpublisher/controller/MqController.java
@@ -1,9 +1,13 @@
 package com.example.mqpublisher.controller;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +19,23 @@ public class MqController {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
     @GetMapping("/mq/queue")
     public ResponseEntity testPublish(@RequestParam("message") String message,
                                       @RequestParam("topic") String topic){
         rabbitTemplate.convertAndSend(EXCHANGE_NAME, "mqtest.cook."+topic, message);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/create/queue")
+    public ResponseEntity createQueue(@RequestParam("queueName") String queueName,
+                                      @RequestParam("routingKey") String routingKey){
+        Queue queue = new Queue(queueName, true, false, false);
+        Binding binding = new Binding(queueName, Binding.DestinationType.QUEUE, EXCHANGE_NAME, routingKey, null);
+        amqpAdmin.declareQueue(queue);
+        amqpAdmin.declareBinding(binding);
         return ResponseEntity.ok().build();
     }
 
